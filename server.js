a055c8ea289844db01855d832a25ac8d5819b9d0
Dominik Broj <dominik.broj@gmail.com>
2016-06-27 19:27:03 +0200

    change port to make heroku work

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ serverApp['put']('/updateComment', function(req, res) {
 /* RUN SERVER */
 
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
