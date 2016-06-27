31e7ac5f8c5f4b38ffec2ff174a5b016d723247d
Dominik Broj <dominik.broj@gmail.com>
2016-06-27 19:29:55 +0200

    debug server.js

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ serverApp['put']('/updateComment', function(req, res) {
 /* RUN SERVER */
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+serverApp.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, serverApp.settings.env);
 });
